629ba047ae52fecf55f8d27bd6902944c1725f7b
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-09-10 10:30:24 +0200

    Add tailwind to project.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        offBlack: "#1F2937", // For text
+        offWhite: "#F9FAFB", // For background
+      },
+    },
+  },
+  plugins: [],
+};
