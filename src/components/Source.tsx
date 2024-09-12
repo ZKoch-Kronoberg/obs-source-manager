7f10c6e527ab4d1d65eaeb4cd01603c63ee7fe9e
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2024-09-13 00:26:22 +0200

    Add source preview image

--- src/components/Source.tsx
+++ src/components/Source.tsx
@@ -1,6 +1,7 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
 import { OBSSource } from "../shared";
 import { OBSConnectionContext } from "../contexts/OBSConnectionContext";
+import { toast } from "react-toastify";
 
 interface SourceProps {
   source: OBSSource;
@@ -8,7 +9,38 @@ interface SourceProps {
 }
 
 const Source: FunctionComponent<SourceProps> = ({ source, setEnabled }) => {
-  const { isRecording } = useContext(OBSConnectionContext);
+  const { connection, isRecording } = useContext(OBSConnectionContext);
+
+  const [imageData, setImageData] = useState<string>();
+
+  useEffect(() => {
+    const getPreviewImage = async () => {
+      try {
+        if (!connection) {
+          throw new Error("could not connect to webSocket");
+        }
+
+        const response = await connection.call("GetSourceScreenshot", {
+          sourceName: source.sourceName,
+          imageFormat: "jpg",
+          imageWidth: 256,
+          imageCompressionQuality: 25,
+        });
+
+        setImageData(response.imageData);
+      } catch (error) {
+        console.error(error);
+
+        if (error instanceof Error) {
+          toast.error(`Error: ${error.message}`);
+        } else {
+          toast.error("An unexpected error occured");
+        }
+      }
+    };
+
+    getPreviewImage()
+  }, [source, connection]);
 
   function toggleSourceEnabled() {
     setEnabled(source, !source.enabled);
@@ -20,6 +52,7 @@ const Source: FunctionComponent<SourceProps> = ({ source, setEnabled }) => {
         <span className="border-b-2 font-semibold px-2">
           {source.sourceName}
         </span>
+        {imageData? <img className="mx-2" src={`${imageData}`} alt={`preview of the video source ${source.sourceName}`} /> : null}
         <span className="px-2">
           {source.enabled ? "enabled " : "disabled "}
         </span>
