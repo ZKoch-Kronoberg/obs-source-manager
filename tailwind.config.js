f0d025d0c51db3b08ac1ff9015f0d938307222fc
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-10-01 13:30:53 +0200

    Improve focus state styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,19 @@ module.exports = {
       colors: {
         offBlack: "#1F2937", // For text
         offWhite: "#F9FAFB", // For background
+        customFocus: "#3B82F6",
       },
       borderColor: {
         DEFAULT: "#1F2937",
+      },
+      outline: {
+        custom: "2px solid #3B82F6",
+      },
+      variants: {
+        extend: {
+          ringColor: ["focus"], // Enable ring color on focus
+          ringWidth: ["focus"], // Enable ring width on focus
+        },
       },
     },
   },
