05dc6b48aa0f63496108d5227580029f741907bc
ZKoch-Kronoberg <165011923+ZKoch-Kronoberg@users.noreply.github.com>
2024-10-11 12:29:57 +0200

    Update project theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        offBlack: "#1F2937", // For text
-        offWhite: "#F9FAFB", // For background
+        darkGray: "#474747",
+        lightGray: "#EDE7E7",
+        gray: "#777676",
+        offblack: "#141414",
         customFocus: "#3B82F6",
       },
       borderColor: {
-        DEFAULT: "#1F2937",
+        DEFAULT: "#777676",
+      },
+      borderRadius: {
+        sm: "10px",
+        md: "20px",
+        lg: "35px",
+        full: "9999px",
       },
       outline: {
         custom: "2px solid #3B82F6",
+      },
+      fontSize: {
+        h1: "48px",
+        h2: "24px",
+        h3: "18px",
+        p: "16px",
       },
       variants: {
         extend: {
